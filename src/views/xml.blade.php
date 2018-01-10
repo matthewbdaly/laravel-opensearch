4d6e9460086b9a9f61a8cb81b2e50eaabef99f84
Matthew Daly <matthewbdaly@gmail.com>
2018-01-10 21:35:06 +0000

    Corrected template

--- src/views/xml.blade.php
+++ src/views/xml.blade.php
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns:moz="http://www.mozilla.org/2006/browser/search/"
    xmlns="http://a9.com/-/spec/opensearch/1.1/">
-   <ShortName>example.com</ShortName>
-   <Description>Search example.com</Description>
+   <ShortName>{{ $shortname }}</ShortName>
+   <Description>{{ $description }}</Description>
    <InputEncoding>UTF-8</InputEncoding>
    <Url method="get" type="text/html"
-      template="http://example.com/search?q={searchTerms}"/>
+      template="{{ $template }}"/>
 </OpenSearchDescription>
