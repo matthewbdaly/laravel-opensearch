f6eb8be187dcadf5b39e6d16a86e5dc16e4fefe7
Matthew Daly <matthewbdaly@gmail.com>
2018-01-10 20:35:01 +0000

    Initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Hash;
+
+class TestCase extends BaseTestCase
+{
+    use MockeryPHPUnitIntegration;
+
+	protected function getPackageProviders($app)
+	{
+		return ['Matthewbdaly\LaravelPackageBoilerplate\ServiceProvider'];
+	}
+
+	protected function getPackageAliases($app)
+	{
+		return [
+			'Facade' => 'Matthewbdaly\LaravelPackageBoilerplate\Facade'
+		];
+	}
+
+    /**
+     * Creates the application.
+     *
+     * Needs to be implemented by subclasses.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+
+        Hash::setRounds(4);
+
+        return $app;
+    }
+}
