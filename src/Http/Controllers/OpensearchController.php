4f52c566fe8448da84ba4d29e5580b4a28fab023
Matthew Daly <matthewbdaly@gmail.com>
2018-02-11 22:04:27 +0000

    Commented controller

--- src/Http/Controllers/OpensearchController.php
+++ src/Http/Controllers/OpensearchController.php
@@ -7,10 +7,18 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Controller for Opensearch XML file
+ */
 class OpensearchController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * Return Opensearch XML file
+     *
+     * @return Illuminate\Http\Response
+     */
     public function index()
     {
         $content = view('opensearch::xml', config('opensearch'))->render();
